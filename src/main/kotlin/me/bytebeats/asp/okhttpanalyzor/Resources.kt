18de2c8dd7f415d13ee4234a247f4266d8dbce9e
panchen <panchen@tigerbrokers.com>
2021-08-02 11:24:36 +0800

    Initial commit

---
+++ src/main/kotlin/me/bytebeats/asp/okhttpanalyzor/Resources.kt
@@ -0,0 +1,9 @@
+package me.bytebeats.asp.okhttpanalyzor
+
+import com.intellij.icons.AllIcons.Nodes.ResourceBundle
+
+object Resources {
+    fun getString(name: String): String {
+        return ""
+    }
+}
---
+++ src/main/kotlin/me/bytebeats/asp/okhttpanalyzor/System.kt
@@ -0,0 +1,9 @@
+package me.bytebeats.asp.okhttpanalyzor
+
+object System {
+    fun copyToClipBoard(text: String?) {
+        text?.let {
+//            val stringSelection = StringSelection(it)
+        }
+    }
+}
